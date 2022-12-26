02edd49991d02df86d241b2d56c61fa5eccb6afe
tunalime <mfethi96@gmail.com>
2022-12-26 18:07:32 +0300

    login feature removed.

--- src/main/java/com/example/gpacalculator/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gpacalculator.repository;
-
-import com.example.gpacalculator.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-
-    User getUserById(Long id);
-}
